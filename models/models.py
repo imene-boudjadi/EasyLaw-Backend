0decddfdebf99d62eedaca4b1d85eea21a9d8c4d
nina <102603170+thaninamck@users.noreply.github.com>
2024-04-24 15:59:46 +0100

    models updated

--- models/models.py
+++ models/models.py
@@ -1,46 +1,74 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from .. import db
-from sqlalchemy import func
 
-class Users(db.Model):
-    __tablename__ = "Users"
-    id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(255), nullable=False)
-    LastName = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+class infoContact(db.Model):
+    __tablename__="infoContact"
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    designation=db.Column(db.String)
+    value=db.Column(db.String)
 
-    def __repr__(self):
-        return f'<User {self.firstName} {self.id}>'
-
-
-class Funds(db.Model):
-    __tablename__ = "Funds"
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Numeric(10, 2))
-    userId = db.Column(db.Integer, db.ForeignKey("Users.id"), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    user = db.relationship("Users", backref="funds")
-
-    @property
-    def serialize(self):
+    def to_dict(self):
         return {
-            "id": self.id,
-            "amount": self.amount,
-            "created_at": self.created_at
+            'id': self.id,
+            'designation': self.designation,
+            'value': self.value
         }
+    
+  
+class Acteur(db.Model):
+    __tablename__ = "Acteur"
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    userName = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64))
+    infoContact = db.Column(db.Integer, db.ForeignKey('infoContact.id'))
 
-class Law(db.Model):
-    __tablename__ = "law"
+    role = db.Column(db.String)
+    deleted = db.Column(db.Boolean)
+    Email = db.Column(db.String(120), index=True, unique=True)
 
-    idLaw = db.Column(db.Integer, primary_key=True)
-    wizara = db.Column(db.String(255))
-    sujet = db.Column(db.Text)
-    type = db.Column(db.String(255))
-    num = db.Column(db.String(255))
-    date = db.Column(db.String(255))
-    num_jarida = db.Column(db.Integer)
-    date_jarida = db.Column(db.String(255))
-    page_jarida = db.Column(db.Integer)
-    def __repr__(self):
-        return f'<Law {self.idLaw}>'
+
+class service(db.Model):
+    __tablename__ = "service"
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    nom= db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(100))
+    pic = db.Column(db.String(100))
+
+    
+    
+class AbonnementService(db.Model):
+    __tablename__ = "AbonnementService"
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+   
+    nom = db.Column(db.String)
+    duree = db.Column(db.Integer)
+    description = db.Column(db.String)
+    service_id=db.Column(db.Integer, db.ForeignKey('service.id'))
+
+class PlanTarification(db.Model):
+    __tablename__ = "PlanTarification"
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    nom = db.Column(db.String)
+    type_tarification = db.Column(db.String)
+    tarif=db.Column(db.Integer)
+    monnaire = db.Column(db.String)
+    abonnement_id = db.Column(db.Integer, db.ForeignKey('AbonnementService.id'))
+
+    
+
+
+
+
+class payement(db.Model):
+    __tablename__="payement"
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    abonnement_id = db.Column(db.Integer, db.ForeignKey('AbonnementService.id'))
+    acteur_id = db.Column(db.Integer, db.ForeignKey('Acteur.id'))
+    plan_tarification = db.Column(db.Integer, db.ForeignKey('PlanTarification.id'))
+
+    moyen_paiement=db.Column(db.String)
+    date_paiement=db.Column(db.String)
+
+   
+
